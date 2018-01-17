66c5d75f8b134f45058b0749d9322c1758bee9d2
Kaotic <Kaotic-13@hotmail.fr>
2018-01-17 23:20:55 +0100

    Update offsets

--- Main.cs
+++ Main.cs
@@ -31,8 +31,8 @@ namespace CCleanerBiosUpdater
         private delegate void AsyncWrite(String Text);
         private delegate void AsyncClear();
         public static Boolean ProgramRunning = false;
-        private static Int64 CCleaner_OffGame = 0x14415E900;
-        private Int64 CCleaner_OffGraphics = 0x14415F688;
+        private static Int64 CCleaner_OffGame = 0x14417CFA0;
+        private Int64 CCleaner_OffGraphics = 0x14417DD38;
         private static IntPtr GameHandle;
         private Structures.CCleaner_Margin GameMargin;
         private static CCleanerBios CCleanerBiosReader = new CCleanerBios();
